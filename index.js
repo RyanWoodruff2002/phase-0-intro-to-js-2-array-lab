2b7f1ccf5ef44499dd36b4ed38d1d40dd9807271
Ryan Woodruff <ryanwoodruff2002@gmail.com>
2022-07-01 10:52:21 -0400

    commit tree 7ca95ebac9400fe751a4c1e49b75febf35ad22c0

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution here!
+
+const cats = ["Milo", 'Otis', 'Garfield'];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(name) {
+    cats.pop(name);
+}
+
+function destructivelyRemoveFirstCat(name) {
+    cats.shift(name);
+}
+
+function appendCat(name) {
+    return [...cats].push(name);
+}
